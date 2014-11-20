4852b78c7a76c5f3f2b6715e025d4905a6251f37
unknown <fvmacchi@ECE-FPGA23.NEXUS.UWATERLOO.CA>
2014-11-20 13:41:40 -0500

    changed movement with timers so that velocity is not dependent on framerate, as well as added code to debounce interupts

--- proj5/main.c
+++ proj5/main.c
@@ -17,6 +17,7 @@ OS_MUT listMut, drawMut;
 linked_list_t ball_list;
 
 volatile int createball = 0;
+volatile double lastInterupt;
 
 __task void newBall( void *pointer ) {
 	ball_t ball;
@@ -24,7 +25,7 @@ __task void newBall( void *pointer ) {
 	int x, y, width, height;
 	volatile double lastTime = os_time_get();
 	volatile double dt = 0;
-	ball_init(&ball, 11, Blue, 0, 0, 1, 1);
+	ball_init(&ball, 31, Blue, 0, 0, 1, 1);
 	os_mut_wait(&listMut, 0xffff);
 	list_add(&ball_list, &ball);
 	os_mut_release(&listMut);
@@ -40,15 +41,15 @@ __task void newBall( void *pointer ) {
 		bitmap_circle(bitmap, ball.size, ball.colour);
 		x0 = ball.x;
 		y0 = ball.y;
-		ball.x += ball.velx;
-		ball.y += ball.vely;
+		ball.x += ball.velx*dt;
+		ball.y += ball.vely*dt;
 		if(ball.x < 0 || ball.x + ball.size > SCREEN_WIDTH) {
 			ball.velx = -ball.velx;
-			ball.x += ball.velx;
+			ball.x += ball.velx*dt;
 		}
 		if(ball.y < 0 || ball.y + ball.size > SCREEN_HEIGHT) {
 			ball.vely = -ball.vely;
-			ball.y += ball.vely;
+			ball.y += ball.vely*dt;
 		}
 		GLCD_Bitmap(ball.x,ball.y,ball.size,ball.size,(unsigned char *)bitmap);
 		bitmap_clear(bitmap, ball.size);
@@ -58,6 +59,23 @@ __task void newBall( void *pointer ) {
 			GLCD_Bitmap(x0,y0,ball.size,ball.size, (unsigned char *)bitmap);
 		}
 		else {
+			y = y0;
+			height = ball.size;
+			if(x0 < ball.x) {
+				x = x0;
+				width = ball.x - x0 + 1;
+			}
+			else {
+				x = ball.x + ball.size;
+				width = x0 - ball.x + 1;
+			}
+			if(x + width > SCREEN_WIDTH) {
+				width -= x + width - SCREEN_WIDTH;
+			}
+			if(width > 0) {
+				GLCD_Bitmap(x,y,width,height,(unsigned char *)bitmap);
+			}
+			
 			y = y0;
 			height = ball.size;
 			if(x0 < ball.x) {
@@ -97,7 +115,7 @@ void EINT3_IRQHandler( void ) {
 	// Check whether the interrupt is called on the falling edge. GPIO Interrupt Status for Falling edge.
 	if ( LPC_GPIOINT->IO2IntStatF && (0x01 << 10) ) {
 		LPC_GPIOINT->IO2IntClr |= (1 << 10); // clear interrupt condition
-
+		
 		createball = 1;
 	}
 }
@@ -105,10 +123,12 @@ void EINT3_IRQHandler( void ) {
 __task void physics( void ) {
 	ball_t *ball = NULL;
 	while(1) {
-		if(createball) {
+		if(createball && os_time_get() - lastInterupt > 25) {
+			lastInterupt = os_time_get();
 			createball = 0;
 			os_tsk_create_ex(newBall, 10, NULL);
 		}
+		createball = 0;
 		/*
 		os_mut_wait(&listMut, 0xffff);
 		{
@@ -130,6 +150,7 @@ __task void init_task( void ) {
 	os_mut_wait(&listMut, 0xffff);
 	list_init(&ball_list);
 	os_mut_release(&listMut);
+	lastInterupt = os_time_get();
 	os_tsk_create(physics,10);
 }
 
